82120b2d0b366a3e0f4ef70924e2596ea33f359e
ChristianoDc7 <christianod97@gmail.com>
2021-07-07 14:26:11 +0300

    Add and Configure autoplay on Slide; Remove Pagination

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
     },
   });
