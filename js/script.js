f8bbbbdc04f0f65039be2583f746d75255f48bee
ChristianoDc7 <christianod97@gmail.com>
2021-07-09 13:16:46 +0300

    adding modal for about

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@
       disableOnInteraction: false,
     },
   });
+
+var modalhidden=document.getElementById('hidden');
+var btnshow=document.getElementById('btnshow');
+var btnhide=document.getElementById('btnhide');
+function showMod(){
+  modalhidden.style.display="block";
+  btnhide.style.display="block";
+  btnshow.style.display="none";
+  
+ };
+ function hideMod(){
+  modalhidden.style.display="none";
+  btnshow.style.display="block";
+  btnhide.style.display="none";
+  
+ };
+btnshow.addEventListener("click", showMod);
+btnhide.addEventListener("click", hideMod);
