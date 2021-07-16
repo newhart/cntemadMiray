63bb6b108853749044017d9e9d9728b64174297f
Fanaingo <fanaingorakotomalala@gmail.com>
2021-07-16 10:23:58 +0300

    fix(activity): pull main

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // initialize swiper pagination to the slide card on activity section
 
 var swiper = new Swiper('.swiper-container',{
+	autoPlay: true,
 	slidesPerView: 1,
 	spaceBetween: 30,
 	loop: true,
@@ -9,3 +10,6 @@ var swiper = new Swiper('.swiper-container',{
 		clickable: true,
 	},
 });
+
+console.log(swiper)
+
