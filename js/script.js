25160eefc545fd3a6a1ea72b273b09018245382d
AinaTahina <ainatahinasafidy@gmail.com>
2021-07-22 15:40:57 +0300

    fix(navbar): add animation to the burger menu

--- js/script.js
+++ js/script.js
@@ -12,3 +12,16 @@
       disableOnInteraction: false,
     },
   });
+
+
+/* navbar animation */
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-nav');
+const menuMobile = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+}
+menu.addEventListener('click', menuMobile);
+
+
+
